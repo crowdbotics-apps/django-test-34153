ac8a1ca1d347d50e88856957bd629404118a3ed6
Kyle Ranous <kranous05@gmail.com>
2022-03-14 08:46:49 -0600

    App API functions complete

--- dashboard/serializers.py
+++ dashboard/serializers.py
@@ -1,9 +1,55 @@
 from rest_framework import serializers
 
+from datetime import datetime 
+
 from .models import App
 
 
 class AppSerializer(serializers.ModelSerializer):
+    # Serializer used for GET Endpoint of App Model
+
     class Meta:
         model = App
         fields = ['id', 'name', 'description', 'type', 'framework', 'domain_name', 'screenshot', 'user', 'created_at', 'updated_at']
+
+
+class PostAppSerializer(serializers.ModelSerializer):
+    # Class should be renamed to be more descriptive of what it is used for
+    # Serializer used for POST, PUT, and PATCH endpoints of App Model
+
+    class Meta:
+        model = App
+        fields = ['name', 'description', 'type', 'framework', 'domain_name']
+
+    def create(self, validated_data):
+        """
+            Create New App Entry, User assigned to request.user
+            created_at populated automatically
+        """
+        app = App.objects.create(**validated_data)
+        app.user = self.context['request'].user
+        app.created_at = datetime.now()
+        app.save()
+        return app
+
+    def update(self, instance, validated_data):
+        
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.type = validated_data.get('type', instance.type)
+        instance.framework = validated_data.get('framework', instance.framework)
+        instance.domain_name = validated_data.get('domain_name', instance.domain_name)
+        instance.updated_at = datetime.now()
+        print("TEST!!!!!")
+        instance.save()
+
+        return instance
+
+    def partial_update(self, instance, validated_data):
+
+        instance.update(**validated_data)
+        instance.updated_at = datetime.now()
+        print("PARTIAL TEST!!!!!")
+        instance.save()
+
+        return instance
--- dashboard/viewsets.py
+++ dashboard/viewsets.py
@@ -2,11 +2,9 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 
 from .models import App
-from .serializers import AppSerializer
+from .serializers import AppSerializer, PostAppSerializer
 
-import logging
 
-logger = logging.getLogger(__name__)
 # Create your views here.
 class AppViewSet(viewsets.ModelViewSet):
     """
@@ -15,8 +13,17 @@ class AppViewSet(viewsets.ModelViewSet):
     queryset = App.objects.all()
     serializer_class = AppSerializer
 
-    def apps_list(self, request, *args, **kwargs):
-        app_list = self.get_object()
-        logger.info('TEST!!!!!!!')
 
-        return Response({"TEST"})
+    def get_serializer_class(self):
+
+        if self.request.method == 'GET':
+            return AppSerializer
+
+        elif self.request.method == 'POST':
+            return PostAppSerializer
+
+        elif self.request.method == 'PUT':
+            return PostAppSerializer
+
+        elif self.request.method == 'PATCH':
+            return PostAppSerializer
